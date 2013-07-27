0fe0ec557ae2ab3be171a41d3ea689cec0a83202
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-07-27 12:30:55 +0000

    Add nice glViewImage icon in svg (for freedesktops), icns (for Mac OS X), .desktop file and install.sh script (for freedesktops).
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/glviewimage@12952 afe8dcc5-9094-451b-8773-5f495042a737

--- create_macosx_bundle.sh
+++ create_macosx_bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../scripts/create_macosx_bundle.sh glViewImage glViewImage images/glViewImage.icns \
+../scripts/create_macosx_bundle.sh glViewImage glViewImage desktop/glViewImage.icns \
   "`output_macosx_document_types`"
 
 cp /sw/lib/libpng14.14.dylib glViewImage.app/Contents/MacOS/libpng.dylib
---
+++ desktop/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+# Install glViewImage menu entries, icons, mime types.
+# Based on ../../view3dscene/desktop/install.sh
+
+APP_NAME='glViewImage'
+
+# For non-user install, change this to
+#   SHARE_PREFIX = /usr/local/share
+# For Debian package, you probably want to set this to
+#   SHARE_PREFIX = $(DESTDIR)/usr/share
+SHARE_PREFIX="$HOME"/.local/share
+
+# Install icons:
+mkdir -p "$SHARE_PREFIX"/icons/hicolor/scalable/apps/
+cp -f "$APP_NAME".svg "$SHARE_PREFIX"/icons/hicolor/scalable/apps/
+
+# Install desktop file:
+mkdir -p "$SHARE_PREFIX"/applications/
+cp -f "$APP_NAME".desktop "$SHARE_PREFIX"/applications/
+if which update-desktop-database >/dev/null 2>&1 ; then update-desktop-database -q "$SHARE_PREFIX"/applications/; fi
+
+echo "Installed $APP_NAME (menu entries, icons, mime types): all OK."
