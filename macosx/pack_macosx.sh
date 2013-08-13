4eb59a2a26d110d736ba387029076c85f935a3d7
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-08-13 01:54:51 +0000

    Move Makefile to produce dmg on Mac OS X to scripts/macosx_dmg.makefile. Use it from view3dscene/glViewImage/castle scripts macosx/pack_macosx.sh.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/glviewimage@13020 afe8dcc5-9094-451b-8773-5f495042a737

--- macosx/pack_macosx.sh
+++ macosx/pack_macosx.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eu
 
+# Create Mac OS X bundle, and then dmg (disk image) file to distribute glViewImage.
+# Compile the glViewImage binary before calling this.
+
 . ../../scripts/create_macosx_bundle.sh
 
 create_bundle glViewImage ../glViewImage ../desktop/glViewImage.icns \
@@ -10,3 +13,6 @@ create_bundle glViewImage ../glViewImage ../desktop/glViewImage.icns \
 cd glViewImage.app/Contents/MacOS/
 cp_fink_lib libpng14.14.dylib
 check_libs_not_depending_on_fink glViewImage
+cd ../../../
+
+make -f ../../scripts/macosx_dmg.makefile NAME=glViewImage
