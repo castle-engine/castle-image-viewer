d88420ffec9737be460726cc370047b23e4b128a
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-03-12 01:11:08 +0100

    Do not require ../castle_game_engine/ dir when building.

--- compile.sh
+++ compile.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -eu
 
-cd ../castle_game_engine/
-# Force rebuilding CastleWindow unit with proper backend.
-make --quiet clean-window
+if [ -d ../castle_game_engine/ ]; then
+  cd ../castle_game_engine/
+  # Force rebuilding CastleWindow unit with proper backend.
+  make --quiet clean-window
+  cd ../glviewimage/
+fi
 
 # Compile binary using castle-engine,
 # this is good for Windows to include the icon/versioninfo/manfest in resources.
-cd ../glviewimage/
 castle-engine compile ${CASTLE_ENGINE_TOOL_OPTIONS:-}
