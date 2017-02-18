e659c2b7f52355c76f29b5426ecee48043b96a56
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-02-18 21:33:52 +0100

    Fix pack to use cge-scripts, and not try fink libraries

--- macosx/pack_macosx.sh
+++ macosx/pack_macosx.sh
@@ -4,15 +4,20 @@ set -eu
 # Create Mac OS X bundle, and then dmg (disk image) file to distribute glViewImage.
 # Compile the glViewImage binary before calling this.
 
-. ../../scripts/create_macosx_bundle.sh
+. ../../cge-scripts/create_macosx_bundle.sh
 
 create_bundle glViewImage ../glViewImage ../desktop/glViewImage.icns \
   "`output_macosx_document_types`"
 
 # add libraries from fink
 cd glViewImage.app/Contents/MacOS/
-cp_fink_lib libpng14.14.dylib
+
+# TODO: for now, I don't have fink available.
+# Fortunately, lack of png is harmless now, we read PNG without it.
+#
+# cp_fink_lib libpng14.14.dylib
+
 check_libs_not_depending_on_fink glViewImage
 cd ../../../
 
-make -f ../../scripts/macosx_dmg.makefile NAME=glViewImage
+make -f ../../cge-scripts/macosx_dmg.makefile NAME=glViewImage
