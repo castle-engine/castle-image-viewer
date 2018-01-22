4b37b9df49f0980ade899cbe358627615529bfb0
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-01-22 23:22:44 +0100

    Proper Mac OS X building is now completely automated

--- Makefile
+++ Makefile
@@ -5,5 +5,7 @@ compile:
 # Run also "dircleaner . clean" here to really clean
 .PHONY: clean
 clean:
+	castle-engine clean
+	cd macosx/utils && castle-engine clean
 	rm -f glViewImage glViewImage.exe
-	rm -Rf glViewImage.app macosx/glViewImage.app
+	rm -Rf glViewImage.app macosx/glViewImage.app .DS_Store backup
--- macosx/pack_macosx.sh
+++ macosx/pack_macosx.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 
-# Create Mac OS X bundle, and then dmg (disk image) file to distribute glViewImage.
-# Compile the glViewImage binary before calling this.
+# Compile for Mac OS X (using CGE alternative_castle_window_based_on_lcl package),
+# create Mac OS X bundle (.app),
+# create dmg (disk image) file to distribute.
+
 
 . ../../cge-scripts/create_macosx_bundle.sh
 
+make -C ../ clean
+
+# compile utils/output_macosx_document_types
+echo '--------------------- Compiling utils  --------------------'
+pushd .
+cd utils/
+castle-engine compile
+popd
+
+# compile glViewImage for Mac OS X
+echo '--------------------- Compiling glViewImage  --------------------'
+temporary_change_lpi_to_alternative_castle_window_based_on_lcl ../glViewImage.lpi
+lazbuild ../glViewImage.lpi
+
+# Fail if utils/output_macosx_document_types is missing
+utils/output_macosx_document_types > /tmp/document_types.txt
+
 create_bundle glViewImage ../glViewImage ../desktop/glViewImage.icns \
-  "`../utils/output_macosx_document_types`"
+  "`cat /tmp/document_types.txt`"
 
-# add libraries from fink
-cd glViewImage.app/Contents/MacOS/
+# We used to add now libpng from Fink, but
+# - we don't use Fink anymore
+# - we don't need libpng anymore, FpImage has internal png reader
 
-# TODO: for now, I don't have fink available.
-# Fortunately, lack of png is harmless now, we read PNG without it.
-#
-# cp_fink_lib libpng14.14.dylib
+create_dmg glViewImage
 
-check_libs_not_depending_on_fink glViewImage
-cd ../../../
-
-make -f ../../cge-scripts/macosx_dmg.makefile NAME=glViewImage
-
-echo 'Done OK'
+finish_macosx_pack
