3d6b2b6474a6b3f637c3e4ca3dab0085b3777ee4
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-07-08 16:45:13 +0200

    Revert "Enable TIFF loading using CASTLE_ENABLE_PLATFORM_SPECIFIC_IMAGE_FORMATS, add precompiled libtiff"
    This reverts commit c54a5aa8a903120f2615fff2e8089fff348890de.
    
    This commit is only on extra-image-formats, we don't want to maintain them elsewhere.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Include additional directory in LD_LIBRARY_PATH, to find libtiff
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:libraries/x86_64-linux/"
-./castle-image-viewer "$@"
